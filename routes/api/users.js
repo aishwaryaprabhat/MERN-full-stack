079ca4822898106ad2ea1d43201884e2d614770a
aishwaryaprabhat@gmail.com <aish.prabhat@shopee.com>
2020-03-30 10:50:39 +0800

    Added scripts and code

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar')
 const {check, validationResult} = require('express-validator/check')
+const User = require('../../models/Users')
+
 
 //@route GET api/users
 //@desc Register user
@@ -12,13 +15,38 @@ router.post(
         check('email', 'Please include valid email').isEmail(),
         check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
     ],
-    (req,res)=> 
+    async (req,res)=> 
 {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
     }
+
+    const {name, email, password} = req.body;
+
+    try {
+
+    //See if user exists
+        let user = await User.findOne({email})
+
+        if (user) {
+            res.status(400).json({errors: [{msg: 'User already exixsts'}]});
+        }
+
+    //Get users gravatar
+
+    //Encrypt password
+
+    //Return jsonwebtoken
     res.send('User route');
+        
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+
+    
+    
 });
 
 module.exports = router;
