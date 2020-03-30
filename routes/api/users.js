9451b621ac778b93ca94990eda26f472171b1b07
aishwaryaprabhat@gmail.com <aish.prabhat@shopee.com>
2020-03-30 14:48:25 +0800

    JWT working

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const gravatar = require('gravatar')
 const bcrypt  = require('bcryptjs')
 const {check, validationResult} = require('express-validator/check')
 const User = require('../../models/Users')
+const config = require("config")
+const jwt = require('jsonwebtoken')
 
 
 //@route GET api/users
@@ -50,6 +52,21 @@ router.post(
         await user.save();
 
         //Return jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            {expiresIn: 360000},
+            (err, token)=> {
+                if (err) throw err;
+                res.json({token})
+            }
+        )
         res.send('User route');
         
     } catch (error) {
