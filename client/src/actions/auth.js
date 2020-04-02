f471b9488c60a716ebd7ad98f83c47ced1ca6775
aishwaryaprabhat@gmail.com <aish.prabhat@shopee.com>
2020-04-02 11:47:09 +0800

    working register and alerts state change

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,9 +1,34 @@
 import axios from 'axios';
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED, 
+    AUTH_ERROR
 } from './types';
 import {setAlert} from './alert'
+import setAuthToken from '../utils/setAuthToken'
+
+//Load User
+export const loadUser = ()=> async dispatch => {
+    //check if there is a toker and put it in a global header
+    if(localStorage.token){
+        setAuthToken(localStorage.token);
+    }
+
+    try {
+        const res = await axios.get('/api/auth');
+
+        dispatch({
+            type: USER_LOADED,
+            payload: res.data
+        })
+    } catch (error) {
+        dispatch({
+            type: AUTH_ERROR
+        })
+    }
+}
+
 
 //Register User
 export const register = ({name, email, password}) => async dispatch =>{
@@ -17,6 +42,35 @@ export const register = ({name, email, password}) => async dispatch =>{
 
     try {
         const res = await axios.post('/api/users', body, config);
+        dispatch({
+            type: REGISTER_SUCCESS,
+            payload: res.data
+        })
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        if(errors){
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: REGISTER_FAIL
+        })
+    }
+}
+
+//Login User
+export const login = (email, password) => async dispatch =>{
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const body = JSON.stringify({email, password});
+
+    try {
+        const res = await axios.post('/api/auth', body, config);
         dispatch({
             type: REGISTER_SUCCESS,
             payload: res.data
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,5 +1,8 @@
 import {
-    REGISTER_SUCCESS, REGISTER_FAIL
+    REGISTER_SUCCESS, 
+    REGISTER_FAIL, 
+    USER_LOADED, 
+    AUTH_ERROR
 } from '../actions/types'
 
 const initialState = {
@@ -22,6 +25,7 @@ export default function(state=initialState, action) {
                 isAuthenticated:true,
                 loading:false
             }
+        case AUTH_ERROR:
         case REGISTER_FAIL:
             localStorage.removeItem('token');
             return {
@@ -30,6 +34,15 @@ export default function(state=initialState, action) {
                 isAuthenticated:false,
                 loading:false
             }
+
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: payload
+            }
+            
         default:
             return state;
     }
